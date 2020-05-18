bc4734a5ca37a01ebbd302525e2d0ade7133e40c
Alexey Dubrov <vhsw@ya.ru>
2020-05-18 23:48:28 +0300

    add flask server

---
+++ server.py
@@ -0,0 +1,26 @@
+"""Flask weather API"""
+from flask import Flask, redirect, request, json
+from weather import get_current_weather, get_forcast
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def default_city():
+    """Redirect to default city"""
+    return redirect("city/Moscow")
+
+
+@app.route("/city/<city>")
+def weather(city: str):
+    """Get weather in the city"""
+    return json.dumps(get_current_weather(city))
+
+
+@app.route("/city/<city>/forcast")
+def forcast(city: str):
+    """Get weather forcast in the city"""
+    days = request.args.get("days", type=int, default=7)
+    if days < 1 or days > 7:
+        raise ValueError("days must be in 1..7")
+    return json.dumps(get_forcast(city, days))
